2bb8c1df80b87c0fd3196b4500c59b96ead0f339
sanderjson <sanderson.jma@gmail.com>
2020-03-05 15:55:55 -0700

    page transition

--- prepend.js
+++ prepend.js
@@ -1,4 +1,14 @@
 const fs = require('fs')
 const filepath = './src/assets/main.js'
 const data = fs.readFileSync(filepath)
-fs.writeFileSync(filepath, 'import "./css/main.css"' + '\r\n' + data)
+fs.writeFileSync(
+  filepath,
+  'import "./css/main.css";' +
+    '\r\n' +
+    'import barba from "@barba/core";' +
+    '\r\n' +
+    'import {gsap} from "gsap";' +
+    '\r\n' +
+    data
+)
+
