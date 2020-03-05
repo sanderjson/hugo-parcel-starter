f276252ff2dcbff35f3535afe3e6c54e8d5114e1
sanderjson <sanderson.jma@gmail.com>
2020-03-05 16:06:32 -0700

    test

--- prepend.js
+++ prepend.js
@@ -3,8 +3,8 @@ const filepath = './src/assets/main.js'
 const data = fs.readFileSync(filepath)
 fs.writeFileSync(
   filepath,
-  'import "./css/main.css";' +
-    '\r\n' +
+//   'import "./css/main.css";' +
+//     '\r\n' +
     'import barba from "@barba/core";' +
     '\r\n' +
     'import {gsap} from "gsap";' +
